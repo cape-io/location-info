ab6c109cd7c4e87d74005e6b1498c993dacd0f10
Kai Curry <kai@cape.io>
2017-04-18 16:00:34 -0400

    actions and reducer

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,40 @@
+import { flip, isArray, map, unary } from 'lodash'
+import { createAction } from 'cape-redux'
+import { idString } from './utils'
 // addRoute
 
+// Make and set new routes.
+// @id is a machine readable string for the route.
+// @path is a path string. See url-pattern module for possible options.
+// @props object
+//   `position`.
+function addRoutePayload(id, path, props = {}) {
+  idString(id)
+  // Make our object that represents a route.
+  // Return what we created.
+  return {
+    // Apply any of the props.
+    ...props,
+    id,
+    path: path || id, // path is not required. Default to use the id.
+  }
+}
+
+export const ADD_ROUTE = 'location/ADD_ROUTE'
+export const addRoute = createAction(ADD_ROUTE, addRoutePayload)
+
 // addRoutes
+// When you need an event simpler way to create routes.
+// Key of object is the route id. Value is the route path template string.
+export function addRoutesPayload(routeObject) {
+  if (isArray(routeObject)) {
+    return map(routeObject, unary(addRoutePayload))
+  }
+  return map(routeObject, flip(addRoutePayload))
+}
+export const ADD_ROUTES = 'location/ADD_ROUTES'
+export const addRoutes = createAction(ADD_ROUTES, addRoutesPayload)
 
 // deleteRoute
+export const DEL_ROUTE = 'location/DEL_ROUTE'
+export const delRoute = createAction(DEL_ROUTE, idString)
---
+++ src/reducer.js
@@ -0,0 +1,38 @@
+import { delAt, reduce, setIn } from 'cape-lodash'
+import { get } from 'lodash/fp'
+import { createReducer } from 'cape-redux'
+import { getPath } from './utils'
+import { ADD_ROUTE, ADD_ROUTES, DEL_ROUTE } from './actions'
+
+export const ROUTE_PREFIX = 'route'
+
+export const defaultState = {
+  config: {
+    trailingSlash: true,
+  },
+  [ROUTE_PREFIX]: {
+  },
+}
+export const trailingSlash = get('config.trailingSlash')
+
+export function delRoute(state, payload) {
+  return delAt([ROUTE_PREFIX, payload], state)
+}
+export function setRoute(state, { id, path, ...rest }) {
+  if (state[ROUTE_PREFIX][id]) {
+    throw new Error(`Already have a route with the id of "${id}".`)
+  }
+  return setIn([ROUTE_PREFIX, id], {
+    ...rest, id, path: getPath(trailingSlash(state), id, path),
+  })
+}
+export function setRoutes(state, payload) {
+  return reduce(payload, setRoute, state)
+}
+export const reducers = {
+  [ADD_ROUTE]: setRoute,
+  [ADD_ROUTES]: setRoutes,
+  [DEL_ROUTE]: delRoute,
+}
+const reducer = createReducer(reducers)
+export default reducer
