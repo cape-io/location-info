5d06c283d3959d51e438157d042e83979375e346
Kai Curry <kai@cape.io>
2017-04-18 21:47:30 -0400

    basic selector

--- src/select.js
+++ src/select.js
@@ -1,3 +1,11 @@
-import { get } from 'lodash/fp'
+import { get, map } from 'lodash/fp'
+import { setField } from 'cape-lodash'
+import { select } from 'cape-select'
+import { createSelector } from 'reselect'
+import Pattern from 'url-pattern'
 
-export const selectLocInfo = get('locInfo')
+export const getLocInfo = get('locInfo')
+export const getRoutes = select(getLocInfo, 'route')
+
+export const addPattern = setField('pattern', ({ path, options }) => new Pattern(path, options))
+export const selectRoutes = createSelector(getRoutes, map(addPattern))
