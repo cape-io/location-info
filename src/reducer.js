94a50729bf61d239e12240c7c26965112961f04c
Kai Curry <kai@cape.io>
2017-04-18 23:18:52 -0400

    v4.0.2 test reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,5 @@
-import { delAt, reduce, setIn } from 'cape-lodash'
+import { reduce } from 'lodash'
+import { delAt, setIn } from 'cape-lodash'
 import { createReducer } from 'cape-redux'
 import { getPath } from './utils'
 import { ADD_ROUTE, ADD_ROUTES, DEL_ROUTE } from './actions'
@@ -18,7 +19,7 @@ export function setRoute(state, { id, path, ...rest }) {
   if (state[ROUTE_PREFIX][id]) {
     throw new Error(`Already have a route with the id of "${id}".`)
   }
-  return setIn([ROUTE_PREFIX, id], {
+  return setIn([ROUTE_PREFIX, id], state, {
     ...rest, id, path: getPath(state.trailingSlash, id, path),
   })
 }
@@ -30,5 +31,5 @@ export const reducers = {
   [ADD_ROUTES]: setRoutes,
   [DEL_ROUTE]: delRoute,
 }
-const reducer = createReducer(reducers)
+const reducer = createReducer(reducers, defaultState)
 export default reducer
---
+++ src/reducer.spec.js
@@ -0,0 +1,33 @@
+import test from 'tape'
+import { addRoutes } from './actions'
+import reducer, { defaultState } from './reducer'
+
+const routes = {
+  colors: '/colors',
+  filmfest: '/details/recPkxpU5hm2lfIWC',
+  details: '/details/:showId',
+  home: '/',
+  me: '/me',
+  studentList: '/students',
+}
+
+test('default', (t) => {
+  const res = reducer(undefined)
+  t.deepEqual(res, defaultState)
+  t.end()
+})
+test('setRoutes', (t) => {
+  const res = reducer(undefined, addRoutes(routes))
+  t.deepEqual(res, {
+    route: {
+      colors: { id: 'colors', path: '/colors' },
+      details: { id: 'details', path: '/details/:showId' },
+      filmfest: { id: 'filmfest', path: '/details/recPkxpU5hm2lfIWC' },
+      home: { id: 'home', path: '/' },
+      me: { id: 'me', path: '/me' },
+      studentList: { id: 'studentList', path: '/students' },
+    },
+    trailingSlash: false,
+  })
+  t.end()
+})
